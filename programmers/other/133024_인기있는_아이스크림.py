bb5fceca960b06da82dd20772a39d45d1ed720ed
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-19 10:29:06 +0900

    [프로그래머스] 인기있는 아이스크림 풀이 제출
    - 문제 ID: 133024
    - 난이도: unknown
    - 제출일: 2026. 01. 19. 10:29:05
    - 작성자: 김수인

--- programmers/other/133024_인기있는_아이스크림.py
+++ programmers/other/133024_인기있는_아이스크림.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/133024
 # 작성자: 김수인
-# 작성일: 2026. 01. 19. 10:27:54
+# 작성일: 2026. 01. 19. 10:29:05
 
 SELECT
     FLAVOR
