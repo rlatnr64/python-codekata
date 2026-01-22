c418a9bf9b54824b973bc5c8993e9c96b5be26a4
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-01-22 10:55:54 +0900

    [프로그래머스] 최댓값 구하기 풀이 제출
    - 문제 ID: 59415
    - 난이도: unknown
    - 제출일: 2026. 01. 22. 10:55:53
    - 작성자: 김수인

--- programmers/other/59415_최댓값_구하기.py
+++ programmers/other/59415_최댓값_구하기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59415
 # 작성자: 김수인
-# 작성일: 2026. 01. 22. 10:51:36
+# 작성일: 2026. 01. 22. 10:55:53
 
 SELECT
     DATETIME as "시간"
