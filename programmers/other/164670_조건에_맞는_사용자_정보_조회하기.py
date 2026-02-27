c5e9e1e1eaf11524e9c79832d89142058b2d51c9
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-02-27 15:24:35 +0900

    [프로그래머스] 조건에 맞는 사용자 정보 조회하기 풀이 제출
    - 문제 ID: 164670
    - 난이도: unknown
    - 제출일: 2026. 02. 27. 15:24:33
    - 작성자: 김수인

--- programmers/other/164670_조건에_맞는_사용자_정보_조회하기.py
+++ programmers/other/164670_조건에_맞는_사용자_정보_조회하기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/164670
 # 작성자: 김수인
-# 작성일: 2026. 02. 27. 15:15:40
+# 작성일: 2026. 02. 27. 15:24:33
 
 SELECT
     U.USER_ID,
