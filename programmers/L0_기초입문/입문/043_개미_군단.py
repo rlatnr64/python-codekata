2137fc24617ec61a2698fb07e31a89c6b5ffadbd
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-03-16 16:43:59 +0900

    [프로그래머스] L0 - 개미 군단 (#43)

---
+++ programmers/L0_기초입문/입문/043_개미_군단.py
@@ -0,0 +1,17 @@
+# 개미 군단
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120837
+# 알고리즘: 기초
+# 작성자: 김수인
+# 작성일: 2026. 03. 16. 16:43:59
+
+def solution(hp):
+    answer = 0
+    
+    answer += hp // 5
+    hp %= 5
+    answer += hp // 3
+    hp %= 3
+    answer += hp // 1
+    hp %= 1
+    return answer
