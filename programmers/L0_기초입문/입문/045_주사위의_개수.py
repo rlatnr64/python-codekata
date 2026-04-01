f664108263728f2a660690f118363ca3b98a5cca
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-04-01 17:30:02 +0900

    [프로그래머스] L0 - 주사위의 개수 (#45)

--- programmers/L0_기초입문/입문/045_주사위의_개수.py
+++ programmers/L0_기초입문/입문/045_주사위의_개수.py
@@ -3,18 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120845
 # 알고리즘: 기초
 # 작성자: 김수인
-# 작성일: 2026. 04. 01. 17:28:55
+# 작성일: 2026. 04. 01. 17:30:01
 
 def solution(box, n):
-    answer = 0
-    
-    a = int(box[0]) // n
-    answer += a
-    
-    b = int(box[1]) // n
-    answer *= b
-    
-    c = int(box[2]) // n
-    answer *= c
-    
-    return answer
+    return (box[0] // n) * (box[1] // n) * (box[2] // n)
