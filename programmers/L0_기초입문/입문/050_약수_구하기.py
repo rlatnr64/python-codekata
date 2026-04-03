7a888c2296a1a41331ffd1694458cb446414c0cf
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-04-03 11:59:50 +0900

    [프로그래머스] L0 - 약수 구하기 (#50)

---
+++ programmers/L0_기초입문/입문/050_약수_구하기.py
@@ -0,0 +1,13 @@
+# 약수 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120897
+# 알고리즘: 기초
+# 작성자: 김수인
+# 작성일: 2026. 04. 03. 11:59:49
+
+def solution(n):
+    answer = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            answer.append(i)
+    return answer
