f98c30e8baa9cc321f399c0d27bb6d2765538d09
rlatnr64 <164282102+rlatnr64@users.noreply.github.com>
2026-04-07 12:56:17 +0900

    [프로그래머스] L0 - 가장 큰 수 찾기 (#51)

---
+++ programmers/L0_기초입문/입문/051_가장_큰_수_찾기.py
@@ -0,0 +1,14 @@
+# 가장 큰 수 찾기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120899
+# 알고리즘: 기초
+# 작성자: 김수인
+# 작성일: 2026. 04. 07. 12:56:16
+
+def solution(array):
+    answer = []
+    b = array.index(max(array))
+    array.sort()
+    a = array[-1]
+    answer = a,b
+    return answer
